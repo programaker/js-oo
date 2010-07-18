1f06c4123050fe8f390f17b3ef4bc4dba1342d1e
Marcelo da Silva Gomes <programaker@gmail.com>
2010-07-17 23:41:08 -0300

    adding an "anonymous object" example and changing some names

---
+++ src/anonymous.js
@@ -0,0 +1,8 @@
+var josefino = {
+    name: 'Josefino Camurujipe',
+    age: 65,
+    
+    sayHello: function() {
+        return 'Hello! My name is ' + this.name + ' and I am ' + this.age + ' years old';
+    }
+};
