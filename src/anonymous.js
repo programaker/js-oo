1926eda1e2d9fd7ff104f27b492bd187852eee33
Marcelo da Silva Gomes <programaker@gmail.com>
2010-08-18 10:50:22 -0300

    including chrome and opera in the start server script

--- src/anonymous.js
+++
@@ -1,8 +0,0 @@
-var josefino = {
-    name: 'Josefino Camurujipe',
-    age: 65,
-    
-    sayHello: function() {
-        return 'Hello! My name is ' + this.name + ' and I am ' + this.age + ' years old';
-    }
-};
